e68d79d3fb36112ac4a07757d6c48970685a66ad
AyalaDenul <128472038+AyalaDenul@users.noreply.github.com>
2023-06-10 11:21:49 +0200

    Delete Mediation Figure template.R

--- Mediation Figure template.R
+++
@@ -1,9 +0,0 @@
-library(diagram)
-data <- c(0, "''", 0,0,
-          0, 0, 0,0,
-          "''", "''", 0,"''",
-          0, "''", 0,0)
-M <- matrix (nrow=4, ncol=4, byrow = TRUE, data=data)
-plot<- plotmat (M, pos=c(1,2,1), 
-                name= c( "2","1", "4", "3"), 
-                box.type = "rect", box.size = 0.09, box.prop=0.5,  curve=0, shadow.size = 0)
